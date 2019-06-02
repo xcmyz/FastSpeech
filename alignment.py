ea5aad2489a5a71532042dbcdb82255c845cc50c
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-06-03 01:56:09 +0800

    Add files via upload

--- alignment.py
+++ alignment.py
@@ -1,13 +1,14 @@
 import torch
+import numpy as np
 
 
 def get_alignment(text, pos):
     """ You need a reliable model for alignment """
     """ For test, Multiply three there """
 
-    test_out = torch.zeros(pos.size(0), pos.size(1))
-    for i_batch in range(pos.size(0)):
-        for i_ele in range(pos.size(1)):
+    test_out = torch.zeros(np.shape(pos)[0], np.shape(pos)[1])
+    for i_batch in range(np.shape(pos)[0]):
+        for i_ele in range(np.shape(pos)[1]):
             if pos[i_batch][i_ele] != 0:
                 test_out[i_batch][i_ele] = 3.0
 

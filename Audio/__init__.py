97cda00da7736a102a569f66afb7d31411b66108
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-10-23 07:53:59 -0700

    Add files via upload

---
+++ Audio/__init__.py
@@ -0,0 +1,4 @@
+import Audio.hparams
+import Audio.tools
+import Audio.stft
+import Audio.audio_processing
---
+++ Audio/hparams.py
@@ -0,0 +1,8 @@
+max_wav_value = 32768.0
+sampling_rate = 22050
+filter_length = 1024
+hop_length = 256
+win_length = 1024
+n_mel_channels = 80
+mel_fmin = 0.0
+mel_fmax = 8000.0

e908924fa31c218082e3d43189e4a849f5611215
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-10-23 07:51:05 -0700

    Add files via upload

--- text/symbols.py
+++ text/symbols.py
@@ -4,8 +4,8 @@
 Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from text import cmudict
 
+from text import cmudict
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
@@ -15,4 +15,5 @@ _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad] + list(_special) + list(_punctuation) + \
+    list(_letters) + _arpabet

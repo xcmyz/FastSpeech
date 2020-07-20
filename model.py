1f5aa2a33be2e7fab3a9c2fb8666a08c1240bdcc
xcmyz <xcmyz@outlook.com>
2020-07-20 16:58:17 +0800

    Update postnet in FastSpeech

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
+import hparams as hp
+import utils
 
 from transformer.Models import Encoder, Decoder
 from transformer.Layers import Linear, PostNet
-from modules import LengthRegulator
-import hparams as hp
+from modules import LengthRegulator, CBHG
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -20,7 +22,15 @@ class FastSpeech(nn.Module):
         self.decoder = Decoder()
 
         self.mel_linear = Linear(hp.decoder_output_size, hp.num_mels)
-        self.postnet = PostNet()
+        self.postnet = CBHG(hp.num_mels, K=8,
+                            projections=[256, hp.num_mels])
+        self.last_linear = Linear(hp.num_mels * 2, hp.num_mels)
+
+    def mask_tensor(self, mel_output, position, mel_max_length):
+        lengths = torch.max(position, -1)[0]
+        mask = ~utils.get_mask_from_lengths(lengths, max_len=mel_max_length)
+        mask = mask.unsqueeze(-1).expand(-1, -1, mel_output.size(-1))
+        return mel_output.masked_fill(mask, 0.)
 
     def forward(self, src_seq, src_pos, mel_pos=None, mel_max_length=None, length_target=None, alpha=1.0):
         encoder_output, _ = self.encoder(src_seq, src_pos)
@@ -33,9 +43,15 @@ class FastSpeech(nn.Module):
             decoder_output = self.decoder(length_regulator_output, mel_pos)
 
             mel_output = self.mel_linear(decoder_output)
-            mel_output_postnet = self.postnet(mel_output) + mel_output
+            mel_output = self.mask_tensor(mel_output, mel_pos, mel_max_length)
+            residual = self.postnet(mel_output)
+            residual = self.last_linear(residual)
+            mel_postnet_output = mel_output + residual
+            mel_postnet_output = self.mask_tensor(mel_postnet_output,
+                                                  mel_pos,
+                                                  mel_max_length)
 
-            return mel_output, mel_output_postnet, duration_predictor_output
+            return mel_output, mel_postnet_output, duration_predictor_output
         else:
             length_regulator_output, decoder_pos = self.length_regulator(encoder_output,
                                                                          alpha=alpha)
@@ -43,9 +59,11 @@ class FastSpeech(nn.Module):
             decoder_output = self.decoder(length_regulator_output, decoder_pos)
 
             mel_output = self.mel_linear(decoder_output)
-            mel_output_postnet = self.postnet(mel_output) + mel_output
+            residual = self.postnet(mel_output)
+            residual = self.last_linear(residual)
+            mel_postnet_output = mel_output + residual
 
-            return mel_output, mel_output_postnet
+            return mel_output, mel_postnet_output
 
 
 if __name__ == "__main__":

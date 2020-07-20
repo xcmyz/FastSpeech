9c61770a8eab2cdb1455e1c456b8acc7b52a37bd
xcmyz <xcmyz@outlook.com>
2020-07-20 15:38:16 +0800

    Update hyper parameter

--- hparams.py
+++ hparams.py
@@ -1,11 +1,6 @@
-from text import symbols
-
-# Text
-text_cleaners = ['english_cleaners']
-
 # Mel
-n_mel_channels = 80
 num_mels = 80
+text_cleaners = ['english_cleaners']
 
 # FastSpeech
 vocab_size = 1024
@@ -33,13 +28,13 @@ duration_predictor_kernel_size = 3
 dropout = 0.1
 
 # Train
-alignment_path = "./alignments"
 checkpoint_path = "./model_new"
 logger_path = "./logger"
 mel_ground_truth = "./mels"
+alignment_path = "./alignments"
 
-batch_size = 64
-epochs = 1000
+batch_size = 32
+epochs = 2000
 n_warm_up_step = 4000
 
 learning_rate = 1e-3
@@ -47,6 +42,8 @@ weight_decay = 1e-6
 grad_clip_thresh = 1.0
 decay_step = [500000, 1000000, 2000000]
 
-save_step = 1000
+save_step = 3000
 log_step = 5
 clear_Time = 20
+
+batch_expand_size = 32
